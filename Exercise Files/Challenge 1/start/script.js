9ef2573a1f81ac1b239a03cc2441e2ce8e4db69f
Nuzair Ibrahim <nuzairb@gmail.com>
2018-10-27 17:33:54 +0500

    Bad solution to problem

--- Exercise Files/Challenge 1/start/script.js
+++ Exercise Files/Challenge 1/start/script.js
@@ -9,12 +9,12 @@ Use ES6 whenever possible.
 "use strict";
 
 // Write code here
-
-sum(1, 3);
+function sum(x, y) {
+    return x + y;
+}
+console.log(sum(1, 3));
 //4
-
-sum(10, 20, 5);
+console.log(sum(10, 20, 5));
 //35
-
-sum(2, 5, 80, 1, 10, 12);
+console.log(sum(2, 5, 80, 1, 10, 12));
 //110
