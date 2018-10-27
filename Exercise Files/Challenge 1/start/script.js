12a9d5673f8688c2ced33bc92330220fdbff951d
Nuzair Ibrahim <nuzairb@gmail.com>
2018-10-27 18:27:54 +0500

    solve the challenge - 1 Sum Numbers

--- Exercise Files/Challenge 1/start/script.js
+++ Exercise Files/Challenge 1/start/script.js
@@ -9,8 +9,12 @@ Use ES6 whenever possible.
 "use strict";
 
 // Write code here
-function sum(x, y) {
-    return x + y;
+function sum() {
+    var s = 0;
+    for (var i = 0; i < arguments.length; i++) {
+        s += arguments[i];
+    }
+    return s;
 }
 console.log(sum(1, 3));
 //4
